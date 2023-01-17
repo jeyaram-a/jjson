89480557a44e5b1eb8352b86e023755694c5dc9c
jeyar <jeyarambe2018@gmail.com>
2023-01-17 13:26:29 +0530

    Adds base test cases

---
+++ src/test/java/org/j/jjson/parser/JJSonParserTest.java
@@ -0,0 +1,24 @@
+package org.j.jjson.parser;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JJSonParserTest {
+
+    Path getPath(String fileName) throws URISyntaxException {
+        URL res = getClass().getClassLoader().getResource(fileName);
+       return Paths.get(res.toURI()).toFile().toPath();
+    }
+    @Test
+    void test1() throws URISyntaxException, IOException {
+        var parsed = JJSonParser.parse(getPath("narcos.json"));
+        assert parsed.isJsonObject();
+    }
+}
